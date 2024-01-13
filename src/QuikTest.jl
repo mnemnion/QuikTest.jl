e2daa32e6a73d5a74432e2bb7d5dad6fe7316e2d
Sam Atman <atmanistan@gmail.com>
2024-01-13 16:36:29 -0500

    Begin QuikTest

--- src/QuikTest.jl
+++ src/QuikTest.jl
@@ -1,5 +1,34 @@
 module QuikTest
 
-# Write your package code here.
+using ToggleMenus
+
+export menu  # for now
+
+const header::String = "Press t for test (✅), s for snapshot (📸) g for garbage (🗑 ), G to clear"
+const settings::Vector{Char} = ['t', 's', 'g']
+const icons::Vector{String} = ["✅", "📸", "🗑 "]
+
+function onkey(menu::ToggleMenu, i::UInt32)
+    if Char(i) == 'G'
+        selected = copy(menu.selections)
+        removals = []
+        for (idx, selected) in menu.selections |> enumerate
+            if selected == 'g'
+                push!(removals, idx)
+            end
+        end
+        for remove in reverse(removals)
+            deleteat!(menu.options, remove)
+            deleteat!(menu.selections, remove)
+        end
+    end
+    if isempty(menu.options)
+        return true
+    else
+        return false
+    end
+end
+
+menu = ToggleMenuMaker(header, settings, icons, keypress=onkey)
 
 end
