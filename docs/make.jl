bdc0965bb4fa1cf72c85686f40b67037c457b81e
Sam Atman <atmanistan@gmail.com>
2024-03-09 11:49:56 -0500

    Docs skeleton
    QuikTest predates adding `standard_template` to startup.jl, so I
    need to hand-roll a bunch of stuff I've largely forgotten how to
    set up.

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using QuikTest
+using Documenter
+
+DocMeta.setdocmeta!(QuikTest, :DocTestSetup, :(using QuikTest); recursive=true)
+
+makedocs(;
+    modules=[QuikTest],
+    authors="Sam Atman <atmanistan@gmail.com> and contributors",
+    sitename="QuikTest.jl",
+    format=Documenter.HTML(;
+        canonical="https://mnemnion.github.io/QuikTest.jl",
+        edit_link="trunk",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/mnemnion/QuikTest.jl",
+    devbranch="trunk",
+)
